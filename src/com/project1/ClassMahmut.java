d0fd606991c68df580d59783792762a92251ec23
rustemkavaklik <rustemkavaklik@gmail.com>
2022-04-17 14:23:35 +0100

    added new lines

--- src/com/project1/ClassMahmut.java
+++ src/com/project1/ClassMahmut.java
@@ -4,5 +4,13 @@ public class ClassMahmut {
 
     public static void main(String[] args) {
         System.out.println("new project");
+        //code here
+        System.out.println("I added this line after you");
+        String name= "Rustem Kavaklik";
+        if (name.equals(name.toUpperCase())){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
     }
 }
