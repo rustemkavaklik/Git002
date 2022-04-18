593fa2973e1e33691f2415f6a3caf1895ec5d360
mahmut <mcuvoglu@hotmail.com>
2022-04-18 13:12:17 +0100

    mahmut last change

--- src/com/project1/ClassRustem.java
+++ src/com/project1/ClassRustem.java
@@ -3,5 +3,6 @@ package com.project1;
 public class ClassRustem {
     public static void main(String[] args) {
         System.out.println("new project started");
+        System.out.println("Hello everyone");
     }
 }
